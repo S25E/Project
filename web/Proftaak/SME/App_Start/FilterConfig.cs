e54ed9ead50a451f611f5c56373a46991c39d229
Marijn <info@marijnverwegen.nl>
2014-05-14 21:42:47 +0200

    Naam van namespace aangepast.

--- web/Proftaak/SME/App_Start/FilterConfig.cs
+++ web/Proftaak/SME/App_Start/FilterConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 
-namespace mvcWeb
+namespace SME
 {
     public class FilterConfig
     {
--- web/Proftaak/SME/Controllers/HomeController.cs
+++ web/Proftaak/SME/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace mvcWeb.Controllers
+namespace SME.Controllers
 {
     /// <summary>
     ///  
