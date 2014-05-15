38a7d1e4798be6765ebb64cf6c4a2853cb299a6c
Marijn <info@marijnverwegen.nl>
2014-05-15 20:57:13 +0200

    Het setup-project in eerste instantie. Na overleg met software-docent moest er een andere opzet gebruikt worden.

--- web/Proftaak/MVC OUD/App_Start/RouteConfig.cs
+++ web/Proftaak/MVC OUD/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SME
+namespace MVCOUD
 {
     public class RouteConfig
     {
--- web/Proftaak/MVC OUD/App_Start/WebApiConfig.cs
+++ web/Proftaak/MVC OUD/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace SME
+namespace MVCOUD
 {
     public static class WebApiConfig
     {
