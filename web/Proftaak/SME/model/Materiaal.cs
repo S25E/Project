ccc73151abaa910c8ebc0812458e20b1b775673c
EricDeRegter <ericderegter@gmail.com>
2014-05-16 13:54:00 +0200

    Classes toegevoegd en namespace aangepast

---
+++ web/Proftaak/SME/model/Materiaal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SME
+{
+    class Materiaal
+    {
+        public string Persoonsnaam;
+        public int Persoonnummer;
+        public string Productnaam;
+        public int Barcode;
+
+        /// <summary>
+        /// Constructor, deze wordt gebruik bij het aanmaken van een reservering
+        /// </summary>
+        /// <param name="Persoonsnaam"></param>
+        /// <param name="Persoonnummer"></param>
+        /// <param name="Productnaam"></param>
+        /// <param name="Barcode"></param>
+        public Materiaal(string Persoonsnaam, int Persoonnummer, string Productnaam, int Barcode)
+        {
+            this.Persoonsnaam = Persoonsnaam;
+            this.Persoonnummer = Persoonnummer;
+            this.Productnaam = Productnaam;
+            this.Barcode = Barcode;
+        }
+    }
+}
---
+++ web/Proftaak/SME/model/Reservering.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SME
+{
+    class Reservering
+    {
+        public Hoofdboeker Hoofdboeker;
+        public List<Bijboeker> Bijboekers;
+        public List<Kampeerplaats> Kampeerplaatsen;
+        public int Reserveringsnummer;
+        public bool Betaald;
+        public int AantalPersonen;
+        public List<Materiaal> Materialen;
+
+        public Reservering(Hoofdboeker Hoofdboeker, List<Bijboeker> Bijboekers, List<Kampeerplaats> Kampeerplaatsen, int Reserveringsnummer, bool Betaald, int AantalPersonen, List<Materiaal> Materialen)
+        {
+            this.Hoofdboeker = Hoofdboeker;
+            this.Bijboekers = Bijboekers;
+            this.Kampeerplaatsen = Kampeerplaatsen;
+            this.Reserveringsnummer = Reserveringsnummer;
+            this.Betaald = Betaald;
+            this.AantalPersonen = AantalPersonen;
+            this.Materialen = Materialen;
+        }
+
+        public Reservering(Hoofdboeker Hoofdboeker, List<Bijboeker> Bijboekers, List<Kampeerplaats> Kampeerplaatsen, int Reserveringsnummer, bool Betaald, int AantalPersonen)
+        {
+            this.Hoofdboeker = Hoofdboeker;
+            this.Bijboekers = Bijboekers;
+            this.Kampeerplaatsen = Kampeerplaatsen;
+            this.Reserveringsnummer = Reserveringsnummer;
+            this.Betaald = Betaald;
+            this.AantalPersonen = AantalPersonen;
+
+        }
+
+        //public void AddPersoon(Persoon persoon)
+        //{
+
+        //}
+
+        //public void DeletePersoon(Persoon persoon)
+        //{
+
+        //}
+
+        //public void ZetOpBetaald()
+        //{
+
+        //}
+
+        //public decimal BerekenPrijs()
+        //{
+        //    
+        //}
+    }
+}
