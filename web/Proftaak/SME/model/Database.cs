24942b8644df6f7ec8585d2ecd848e6389424c22
Marijn <info@marijnverwegen.nl>
2014-05-17 14:37:26 +0200

    Database class aangepast.

--- web/Proftaak/SME/model/Database.cs
+++ web/Proftaak/SME/model/Database.cs
@@ -15,7 +15,7 @@ namespace SME
         /// <summary>
         /// Het initiëren van de databaseinstellingen
         /// </summary>
-        public Database()
+        static Database()
         {
             oc = new OracleConnection();
             oc.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SMEConnectionString"].ConnectionString;
@@ -26,7 +26,7 @@ namespace SME
         /// </summary>
         /// <param name="query"></param>
         /// <returns>En een DataTable met de opgehaalde infromatie</returns>
-        public static DataTable GetData(OracleCommand command)
+        public static DataTable GetData(OracleCommand command, Dictionary<string, string> waardes = default(Dictionary<string, string>))
         {
             DataTable dt = new DataTable();
 
@@ -34,7 +34,16 @@ namespace SME
             {
                 oc.Open();
                 command.Connection = oc;
+
+                if (!waardes.Equals(default(Dictionary<string, string>)))
+                {
+                    foreach(KeyValuePair<string, string> waarde in waardes){
+                        command.Parameters.Add(waarde.Key, waarde.Value);
+                    }
+                }
+
                 OracleDataAdapter adapter = new OracleDataAdapter(command);
+
                 adapter.Fill(dt);
             }
             catch(Exception e)
