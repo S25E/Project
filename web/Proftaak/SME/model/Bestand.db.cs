df31394ccec3732153af88c0fa87bc1cbca8e7a0
Marijn <info@marijnverwegen.nl>
2014-05-20 20:55:08 +0200

    Het ophalen van bestanden uit database geüpdatet, zodat het overweg kan met de nieuwe databasestructuur.

--- web/Proftaak/SME/model/Bestand.db.cs
+++ web/Proftaak/SME/model/Bestand.db.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Data;
 
+// NOTE: getBestandenByWhere en rowToBestand kunnen samen worden gevoegd?
+
 namespace SME
 {
     public partial class Bestand 
@@ -14,7 +16,7 @@ namespace SME
         /// <param name="where"></param>
         /// <returns>DataTable met bestanden</returns>
         private static DataTable getBestandenByWhere(string where = ""){
-            return Database.GetData("SELECT * FROM BESTAND LEFT JOIN GELUIDSFRAGMENT ON BESTAND.BESTAND_NUMMER = GELUIDSFRAGMENT.BESTAND_NUMMER LEFT JOIN FILM ON BESTAND.BESTAND_NUMMER = FILM.BESTAND_NUMMER LEFT JOIN BOEK ON BESTAND.BESTAND_NUMMER = BOEK.BESTAND_NUMMER LEFT JOIN AFBEELDING ON BESTAND.BESTAND_NUMMER = AFBEELDING.BESTAND_NUMMER WHERE (SELECT COUNT(1) FROM DISLIKE_LIKE_REPORT WHERE DISLIKE_LIKE_REPORT.BESTAND_NUMMER = BESTAND.BESTAND_NUMMER AND REPORT = 'Y') <= 3" + (where != "" ? " AND " + where : ""));
+            return Database.GetData("SELECT * FROM BESTAND WHERE (SELECT COUNT(1) FROM DISLIKE_LIKE_REPORT WHERE DISLIKE_LIKE_REPORT.BESTAND_ID = BESTAND.BESTAND_ID AND REPORT = 'Y') <= 3" + (where != "" ? " AND " + where : ""));
         }
 
         /// <summary>
@@ -28,7 +30,7 @@ namespace SME
             foreach (DataRow row in getBestandenByWhere().Rows)
             {
                 Bestand bestand = rowToBestand(row);
-                int mapNummer = Convert.ToInt32(row["MAP_NUMMER"]);
+                int mapNummer = Convert.ToInt32(row["MAP_ID"]);
 
                 if (bestanden.ContainsKey(mapNummer))
                     bestanden[mapNummer].Add(bestand);
@@ -66,7 +68,7 @@ namespace SME
         {
             List<Bestand> bestanden = new List<Bestand>();
 
-            foreach (DataRow row in getBestandenByWhere("MAP_NUMMER = " + map.Nummer).Rows)
+            foreach (DataRow row in getBestandenByWhere("MAP_ID = " + map.Nummer).Rows)
                 bestanden.Add(rowToBestand(row));
 
             return bestanden;
@@ -79,11 +81,11 @@ namespace SME
         /// <param name="persoon"></param>
         public static void AddLike(Bestand bestand, Persoon persoon)
         {
-            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
             if (dt.Rows.Count == 0)
-                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (PERSOON_NUMMER, BESTAND_NUMMER, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", 'LIKE', 'N')");
+                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (RFID, BESTAND_ID, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", 'LIKE', 'N')");
             else
-                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET LIKEDISLIKE = 'LIKE' WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET LIKEDISLIKE = 'LIKE' WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
         }
 
         /// <summary>
@@ -93,11 +95,11 @@ namespace SME
         /// <param name="persoon"></param>
         public static void AddDislike(Bestand bestand, Persoon persoon)
         {
-            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
             if (dt.Rows.Count == 0)
-                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (PERSOON_NUMMER, BESTAND_NUMMER, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", 'DISLIKE', 'N')");
+                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (RFID, BESTAND_ID, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", 'DISLIKE', 'N')");
             else
-                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET LIKEDISLIKE = 'DISLIKE' WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET LIKEDISLIKE = 'DISLIKE' WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
         }
 
         /// <summary>
@@ -107,11 +109,11 @@ namespace SME
         /// <param name="persoon"></param>
         public static void AddReport(Bestand bestand, Persoon persoon)
         {
-            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+            DataTable dt = Database.GetData("SELECT * FROM DISLIKE_LIKE_REPORT WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
             if (dt.Rows.Count == 0)
-                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (PERSOON_NUMMER, BESTAND_NUMMER, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", NULL, 'Y')");
+                Database.Execute("INSERT INTO DISLIKE_LIKE_REPORT (RFID, BESTAND_ID, LIKEDISLIKE, REPORT) VALUES (" + persoon.Nummer + ", " + bestand.Nummer + ", NULL, 'Y')");
             else
-                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET REPORT = 'Y' WHERE PERSOON_NUMMER = " + persoon.Nummer + " AND BESTAND_NUMMER = " + bestand.Nummer);
+                Database.Execute("UPDATE DISLIKE_LIKE_REPORT SET REPORT = 'Y' WHERE RFID = " + persoon.Nummer + " AND BESTAND_ID = " + bestand.Nummer);
         }
 
         /// <summary>
@@ -121,43 +123,30 @@ namespace SME
         /// <returns>Het nummer van het toegevoegde bestand</returns>
         public static int AddBestand(Bestand bestand)
         {
-            string type = null;
-            if (bestand is Geluidsfragment)
-                type = "GELUIDSFRAGMENT";
-            else if (bestand is Film)
-                type = "FILM";
-            else if (bestand is Boek)
-                type = "BOEK";
-            else if (bestand is Afbeelding)
-                type = "AFBEELDING";
-
-            int insertedId = Convert.ToInt32(Database.GetData("SELECT bestand1.nextval FROM dual").Rows[0]["NEXTVAL"]);
-            Database.Execute("INSERT INTO BESTAND (BESTAND_NUMMER, DATUM, NAAM, MAP_NUMMER, PERSOON_NUMMER, GROOTTE, OPMERKING, BESTANDSLOCATIE, TYPE) VALUES (@nummer, TO_DATE(@datum, 'SYYYY-MM-DD HH24:MI:SS'), @naam, @map_nummer, @persoon_nummer, @grootte, @opmerking, @bestandlocatie, @type)", new Dictionary<string, object>()
+            int insertId = Convert.ToInt32(Database.GetData("SELECT SEQ_BESTAND.nextval FROM dual").Rows[0]["NEXTVAL"]);
+            Database.Execute("INSERT INTO BESTAND (BESTAND_ID, MAP_ID, NAAM, BESCHRIJVING, EXTENSIE, GROOTTE, RFID, DATUM, PAD, IMGINDEX) VALUES (@nummer, @map_nummer, @naam, @beschrijving, @extensie, @grootte, @rfid, TO_DATE(@datum, 'SYYYY-MM-DD HH24:MI:SS'), @pad, @imgindex)", new Dictionary<string, object>()
             {
-                {"@nummer", insertedId},
-                {"@datum", bestand.UploadDatum.ToString("yyyy-MM-dd HH:mm:ss")},
-                {"@naam", bestand.Bestandsnaam},
+                {"@nummer", insertId},
                 {"@map_nummer", bestand.MapNummer},
-                {"@persoon_nummer", bestand.GeuploadDoor.Nummer},
+                {"@naam", bestand.Naam},
+                {"@beschrijving", bestand.Beschrijving},
+                {"@extensie", bestand.Extensie},
                 {"@grootte", bestand.Grootte},
-                {"@opmerking", bestand.Opmerking},
-                {"@bestandlocatie", bestand.Bestandslocatie},
-                {"@type", type}
+                {"@rfid", bestand.Uploader.Nummer},
+                {"@datum", bestand.Datum.ToString("yyyy-MM-dd HH:mm:ss")},
+                {"@pad", bestand.Pad},
+                {"@imgindex", bestand.Image}
             });
 
-            return insertedId;
+            return insertId;
         }
-
+          
         /// <summary>
         /// Het verwijderen van een bestand
         /// </summary>
         /// <param name="bestand"></param>
         public static void DeleteBestand(Bestand bestand)
         {
-            Database.Execute("DELETE FROM FILM WHERE BESTAND_NUMMER = " + bestand.Nummer);
-            Database.Execute("DELETE FROM GELUIDSFRAGMENT WHERE BESTAND_NUMMER = " + bestand.Nummer);
-            Database.Execute("DELETE FROM BOEK WHERE BESTAND_NUMMER = " + bestand.Nummer);
-            Database.Execute("DELETE FROM AFBEELDING WHERE BESTAND_NUMMER = " + bestand.Nummer);
             Database.Execute("DELETE FROM REACTIE WHERE BESTAND_NUMMER = " + bestand.Nummer);
             Database.Execute("DELETE FROM DISLIKE_LIKE_REPORT WHERE BESTAND_NUMMER = " + bestand.Nummer);
             Database.Execute("DELETE FROM BESTAND WHERE BESTAND_NUMMER = " + bestand.Nummer);
@@ -170,85 +159,20 @@ namespace SME
         /// <returns>Het bestand</returns>
         private static Bestand rowToBestand(DataRow row)
         {
-            string type = row["TYPE"].ToString().ToUpper();
-
-            // Waardes die voor alle types bestanden relevant zijn, definiëren 
-            int nummer = Convert.ToInt32(row["BESTAND_NUMMER"]);
-            string bestandnaam = row["NAAM"].ToString();
-            string bestandslocatie = row["BESTANDSLOCATIE"].ToString();
-            int grootte = Convert.ToInt32(row["GROOTTE"]);
-            string opmerking = row["OPMERKING"].ToString();
-            DateTime uploaddatum = Convert.ToDateTime(row["DATUM"]);
-            int geuploaddoor = Convert.ToInt32(row["PERSOON_NUMMER"]);
-            int mapnummer = Convert.ToInt32(row["MAP_NUMMER"]);
-
-            switch (type)
-            {
-                case "GELUIDSFRAGMENT":
-                    return new Geluidsfragment(
-                        nummer,
-                        bestandnaam,
-                        bestandslocatie,
-                        grootte,
-                        opmerking,
-                        uploaddatum,
-                        geuploaddoor,
-                        mapnummer,
-                        row["ARTIEST"].ToString(),
-                        Convert.ToInt32(row["SPEELDUUR"])
-                    );
-                case "FILM": 
-                    return new Film(
-                        nummer,
-                        bestandnaam,
-                        bestandslocatie,
-                        grootte,
-                        opmerking,
-                        uploaddatum,
-                        geuploaddoor,
-                        mapnummer,
-                        row["MAKER"].ToString(),
-                        Convert.ToInt32(row["FILMDUUR"])
-                    );
-                case "BOEK": 
-                    return new Boek(
-                        nummer,
-                        bestandnaam,
-                        bestandslocatie,
-                        grootte,
-                        opmerking,
-                        uploaddatum,
-                        geuploaddoor,
-                        mapnummer,
-                        Convert.ToInt32(row["AANTAL_PAGINAS"]),
-                        row["SCHRIJVER"].ToString(),
-                        row["TAAL"].ToString(),
-                        row["GENRE"].ToString()
-                    );
-                case "AFBEELDING": 
-                    return new Afbeelding(
-                        nummer,
-                        bestandnaam,
-                        bestandslocatie,
-                        grootte,
-                        opmerking,
-                        uploaddatum,
-                        geuploaddoor,
-                        mapnummer,
-                        row["AFMETING"].ToString()
-                    );
-                default: 
-                    return new Bestand(
-                        nummer,
-                        bestandnaam,
-                        bestandslocatie,
-                        grootte,
-                        opmerking,
-                        uploaddatum,
-                        geuploaddoor,
-                        mapnummer
-                    );
-            }
+            return new Bestand(
+                Convert.ToInt32(row["BESTAND_NUMMER"]),
+                Convert.ToInt32(row["MAP_NUMMER"]),
+                row["BESCHRIJVING"].ToString(),
+                row["NAAM"].ToString(),
+                row["EXTENSIE"].ToString(),
+                Convert.ToInt32(row["GROOTTE"]),
+                Convert.ToInt32(row["RFID"]),
+                Convert.ToDateTime(row["DATUM"]),
+                Convert.ToInt32(row["GEDOWNLOAD"]),
+                Convert.ToInt32(row["RATING"]),
+                row["PAD"].ToString(),
+                Convert.ToInt32(row["IMGINDEX"])
+            );
         }
     }
 }
