7a85c8ebb53fe0e82a1e6b50d362ff9033c834a3
Marijn <info@marijnverwegen.nl>
2014-05-20 21:55:20 +0200

    Aanpassing datakoppeling van Persoon voor nieuwe database.

--- web/Proftaak/SME/model/Persoon.db.cs
+++ web/Proftaak/SME/model/Persoon.db.cs
@@ -13,15 +13,22 @@ namespace SME
 
     public partial class Persoon
     {
+        private static DataTable getPersonenByWhere(string where = "")
+        {
+            return Database.GetData("SELECT PERSOON.*, KLANT.*, KLANT_BETALEND.*, KLANT.RESERVERINGSNUMMER AS k_RESERVERINGSNUMMER, KLANT_BETALEND.RESERVERINGSNUMMER AS kb_RESERVERINGSNUMMER, KLANT_BETALEND.REKENINGNUMMER AS kb_REKENINGNUMMER, MEDEWERKER.REKENINGNUMMER AS m_REKENINGNUMMER FROM PERSOON LEFT JOIN KLANT ON PERSOON.RFID = KLANT.RFID LEFT JOIN KLANT_BETALEND ON PERSOON.RFID = KLANT_BETALEND.RFID LEFT JOIN MEDEWERKER ON PERSOON.RFID = MEDEWERKER.RFID" + (where != "" ? " WHERE " + where : ""));
+        }
+
         /// <summary>
         /// Het ophalen van een bepaald persoon aan de hand van een nummer
         /// </summary>
         /// <param name="nummer"></param>
         /// <returns>De Persoon</returns>
-        public static Persoon GetPersoonBijNummer(int nummer)
+        public static Persoon GetPersoonBijRFID(int rfid)
         {
-            foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE PERSOON_NUMMER = " + nummer).Rows)
+            foreach (DataRow row in getPersonenByWhere("RFID = " + rfid).Rows)
+            {
                 return rowToPersoon(row);
+            }
 
             return null;
         }
@@ -31,13 +38,13 @@ namespace SME
         /// </summary>
         /// <param name="nummers"></param>
         /// <returns>Een Dictonary met personen PERSOON_NUMMER => de Persoon</returns>
-        public static Dictionary<int, Persoon> GetPersonenBijNummers(List<int> nummers)
+        public static Dictionary<int, Persoon> GetPersonenBijRFIDs(List<int> rfids)
         {
             Dictionary<int, Persoon> personen = new Dictionary<int, Persoon>();
 
-            if(nummers.Count > 0)
-                foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE PERSOON_NUMMER IN(" + string.Join(",", nummers) + ")").Rows)
-                    personen.Add(Convert.ToInt32(row["PERSOON_NUMMER"]), this.rowToPersoon(row));
+            if (rfids.Count > 0)
+                foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE RFID IN(" + string.Join(",", rfids) + ")").Rows)
+                    personen.Add(Convert.ToInt32(row["RFID"]), rowToPersoon(row));
 
             return personen;
         }
@@ -51,7 +58,7 @@ namespace SME
         {
             List<Persoon> likes = new List<Persoon>();
 
-            foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE PERSOON_NUMMER IN (SELECT PERSOON_NUMMER FROM DISLIKE_LIKE_REPORT WHERE LIKEDISLIKE = 'LIKE' AND BESTAND_NUMMER = " + bestand.Nummer + ")").Rows)
+            foreach (DataRow row in getPersonenByWhere("RFID IN (SELECT RFID FROM DISLIKE_LIKE_REPORT WHERE LIKEDISLIKE = 'LIKE' AND BESTAND_ID = " + bestand.Nummer + ")").Rows)
                 likes.Add(rowToPersoon(row));
 
             return likes;
@@ -66,7 +73,7 @@ namespace SME
         {
             List<Persoon> likes = new List<Persoon>();
 
-            foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE PERSOON_NUMMER IN (SELECT PERSOON_NUMMER FROM DISLIKE_LIKE_REPORT WHERE LIKEDISLIKE = 'DISLIKE' AND BESTAND_NUMMER = " + bestand.Nummer + ")").Rows)
+            foreach (DataRow row in getPersonenByWhere("RFID IN (SELECT RIFD FROM DISLIKE_LIKE_REPORT WHERE LIKEDISLIKE = 'DISLIKE' AND BESTAND_ID = " + bestand.Nummer + ")").Rows)
                 likes.Add(rowToPersoon(row));
 
             return likes;
@@ -81,7 +88,7 @@ namespace SME
         {
             List<Persoon> likes = new List<Persoon>();
 
-            foreach (DataRow row in Database.GetData("SELECT * FROM PERSOON WHERE PERSOON_NUMMER IN (SELECT PERSOON_NUMMER FROM DISLIKE_LIKE_REPORT WHERE REPORT = 'Y' AND BESTAND_NUMMER = " + bestand.Nummer + ")").Rows)
+            foreach (DataRow row in getPersonenByWhere("RFID IN (SELECT RIFD FROM DISLIKE_LIKE_REPORT WHERE REPORT = 'Y' AND BESTAND_ID = " + bestand.Nummer + ")").Rows)
                 likes.Add(rowToPersoon(row));
 
             return likes;
@@ -94,32 +101,47 @@ namespace SME
         /// <returns>Een Persoon</returns>
         private static Persoon rowToPersoon(DataRow row)
         {
-            string soort = row["SOORT"].ToString().ToUpper();
+            string type = row["TYPE"].ToString().ToUpper();
 
             // Waardes die voor alle types bestanden relevant zijn, definiëren 
-            int nummer = Convert.ToInt32(row["PERSOON_NUMMER"]);
-            string naam = row["NAAM"].ToString();
+            int rfid = Convert.ToInt32(row["RFID"]);
             string wachtwoord = row["WACHTWOORD"].ToString();
+            bool aanwezig = row["AANWEZIG"].ToString() == "Y";
+            string naam = row["NAAM"].ToString();
 
-            switch (soort)
+            switch (type)
             {
-                case "BIJBOEKER":
+                case "Klant":
                     return new Bijboeker(
-                        nummer,
+                        rfid,
+                        wachtwoord,
+                        aanwezig,
                         naam,
-                        wachtwoord
+                        Convert.ToInt32(row["k_RESERVERINGSNUMMER"])
                     );
-                case "HOOFDBOEKER":
+                case "Klant_betalend":
                     return new Hoofdboeker(
-                        nummer,
+                        rfid,
+                        wachtwoord,
+                        aanwezig,
                         naam,
-                        wachtwoord
+                        Convert.ToInt32(row["kb_RESERVERINGSNUMMER"]),
+                        row["STRAAT"].ToString(),
+                        row["POSTCODE"].ToString(),
+                        row["WOONPLAATS"].ToString(),
+                        row["TELEFOON"].ToString(),
+                        row["EMAIL"].ToString(),
+                        row["kb_REKENINGNUMMER"].ToString(),
+                        row["SOFINUMMER"].ToString()
                     );
-                case "MEDEWERKER":
+                case "Medewerker":
                     return new Medewerker(
-                        nummer,
+                        rfid,
+                        wachtwoord,
+                        aanwezig,
                         naam,
-                        wachtwoord
+                        row["FUNCTIE"].ToString(),
+                        row["REKENINGNUMMER"].ToString()
                     );
                 default:
                     throw new InvalidTypePersoonException();
