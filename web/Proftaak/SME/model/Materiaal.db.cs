b0a30fbcb8be43b2d1a029f5ac06a4c710599d35
Jeroenvdwal <jeroen.vanderwal1994@hotmail.com>
2014-05-21 11:55:01 +0200

    Materiaalbeheer toegevoegd

---
+++ web/Proftaak/SME/model/Materiaal.db.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SME
+{
+    public partial class Materiaal
+    {
+        public static void AddMateriaal(Materiaal materiaal)
+        {
+            Database.Execute("INSERT INTO Materiaal (soort, aantal, verhuurprijs, barcode, omschrijving, categorie) VALUES (@soort, @aantal, @verhuurprijs, @barcode, @omschrijving, @categorie)", new Dictionary<string, object>()
+            {
+                {"@soort", materiaal.Soort},
+                {"@aantal", materiaal.Aantal},
+                {"@verhuurprijs", materiaal.Verhuurprijs},
+                {"@barcode", materiaal.Barcode},
+                {"@omschrijving", materiaal.Omschrijving},
+                {"@categorie", materiaal.Categorie}
+            });
+        }
+    }
+}
