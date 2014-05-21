89759f74f64cbcb12b1da35756e66807642114ff
EricDeRegter <ericderegter@gmail.com>
2014-05-21 11:45:20 +0200

    Kampeerplaats databasekoppeling gemaakt.

---
+++ web/Proftaak/SME/model/Kampeerplaats.db.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SME
+{
+	public class Kampeerplaats
+	{
+        public static Kampeerplaats GetKampeerplaatBijNummer(int nummer)
+        {
+            return (Kampeerplaats)null;
+        }
+	}
+}
