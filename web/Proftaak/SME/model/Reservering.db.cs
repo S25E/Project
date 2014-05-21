49f2ca0967940cad5e502222e3403a1b451e4250
EricDeRegter <ericderegter@gmail.com>
2014-05-21 11:39:41 +0200

    TODO-dingen toegevoegd.

---
+++ web/Proftaak/SME/model/Reservering.db.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SME
+{
+    public partial class Reservering
+    {
+        public static List<Reservering> GetReserveringen(){
+
+            List<Reservering> lijst = new List<Reservering>();
+
+            return lijst;
+
+        }
+
+        public static int AddReservering(Reservering reservering)
+        {
+            int insertedId =  Database.GetSequence("RESERVERING");
+            Database.Execute("INSERT INTO RESERVERING (RESERVERINGSNUMMER, DATUM) VALUES (@nummer, TO_DATE(@datum, 'SYYYY-MM-DD HH24:MI:SS'))", new Dictionary<string, object>()
+            {
+                {"@nummer", insertedId},
+                {"@datum",  reservering.Datum.ToString("yyyy-MM-dd HH:mm:ss")}
+            });
+            return insertedId;
+        }
+
+        // NOG TE MAKEN
+        public static void ZetOpBetaald(Reservering reservering)
+        {
+            
+        }
+
+        // NOG TE MAKEN
+        public static void DeleteReservering(Reservering reservering)
+        {
+            // HIER RESERVERING VERWIJDEREN IN DATABASE
+            Persoon.DeletePersoon(reservering.Hoofdboeker);
+            foreach (Bijboeker bijboeker in reservering.Bijboekers)
+            {
+                Persoon.DeletePersoon(bijboeker);
+            }
+        }
+    }
+}
