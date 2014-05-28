8e2d68e0af7a3d9c5f872fb0a1804b252d88eaee
Marijn <info@marijnverwegen.nl>
2014-05-28 13:50:02 +0200

    Dropdownlist vullen.

--- web/Proftaak/SME/Uitleen.aspx.designer.cs
+++ web/Proftaak/SME/Uitleen.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace SME {
     public partial class Uitleen {
         
         /// <summary>
-        /// combobox control.
+        /// DropDownList1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button combobox;
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
         /// LabelNaam control.
@@ -92,14 +92,5 @@ namespace SME {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TbWoonplaats;
-        
-        /// <summary>
-        /// rechts control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton rechts;
     }
 }
