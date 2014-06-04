f9694f330f29c11ed6ea5a809ee58b1b608501ee
Marijn <info@marijnverwegen.nl>
2014-06-04 09:26:45 +0200

    Masterpage + stappen voor reservering toegevoegd

--- web/Proftaak/SME/Reservering/Reservering.master.designer.cs
+++ web/Proftaak/SME/Reservering/Reservering.master.designer.cs
@@ -11,5 +11,14 @@ namespace SME {
     
     
     public partial class NestedMasterPage1 {
+        
+        /// <summary>
+        /// ContentPlaceHolder3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder3;
     }
 }
--- web/Proftaak/SME/Reservering/Stap3.aspx
+++ web/Proftaak/SME/Reservering/Stap3.aspx
@@ -1,1 +1,4 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Reservering/Reservering.master" AutoEventWireup="true" CodeBehind="Stap3.aspx.cs" Inherits="SME.Reservering.Stap3" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder3" runat="server">
+
+</asp:Content>
