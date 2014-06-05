b2fe102cb291e71bc524dda8b2bca608c716f0bc
Marijn <info@marijnverwegen.nl>
2014-06-05 08:55:23 +0200

    Een 5e stap

--- web/Proftaak/SME/Reservering/Reservering.master.designer.cs
+++ web/Proftaak/SME/Reservering/Reservering.master.designer.cs
@@ -49,6 +49,15 @@ namespace SME {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl step4;
         
         /// <summary>
+        /// stap5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl stap5;
+        
+        /// <summary>
         /// ContentPlaceHolder3 control.
         /// </summary>
         /// <remarks>
