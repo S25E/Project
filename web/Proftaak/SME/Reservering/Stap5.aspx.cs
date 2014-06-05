ee148790ce6f47ef4590066b7f3dbb94bfa35240
EricDeRegter <ericderegter@gmail.com>
2014-06-05 12:56:27 +0200

    Alle stappen zijn af, alleen nog toevoegen aan de database

--- web/Proftaak/SME/Reservering/Stap5.aspx.cs
+++ web/Proftaak/SME/Reservering/Stap5.aspx.cs
@@ -11,7 +11,54 @@ namespace SME
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["Stap4"] == null)
+            {
+                Response.Redirect("Stap4.aspx");
+            }
+            else
+            {
+                Hoofdboeker Hoofdboeker = (Hoofdboeker)Session["Stap1"];
+                LabelNaam.Text = Hoofdboeker.Naam;
+                LabelTelefoonnummer.Text = Hoofdboeker.Telefoon;
+                LabelPostocde.Text = Hoofdboeker.Postcode;
+                LabelWoonplaats.Text = Hoofdboeker.Woonplaats;
+                LabelStraat.Text = Hoofdboeker.Straat;
+                LabelEmailadres.Text = Hoofdboeker.Email;
+                LabelRekeningnummer.Text = Hoofdboeker.Rekeningnummer;
+                LabelSofinummer.Text = Hoofdboeker.Sofinummer;
 
+                List<Bijboeker> Bijboekers = (List<Bijboeker>)Session["Stap2"];
+                ListboxBijboekers.DataSource = Bijboekers;
+                ListboxBijboekers.DataTextField = "Naam";
+                ListboxBijboekers.DataBind();
+
+                ListboxKampeerplaats.DataSource = Session["Stap3"];
+                ListboxKampeerplaats.DataBind();
+
+                List<Int32> Barcodes = (List<Int32>)Session["Stap4"];
+                List<Materiaal> Materialen = new List<Materiaal>();
+                foreach(int barcode in Barcodes)
+                {
+                    Materiaal materiaal = Materiaal.GetMateriaalBijBarcode(barcode);
+                    Materialen.Add(materiaal);
+                }
+                
+                Listboxmaterialen.DataSource = Materialen;
+                Listboxmaterialen.DataTextField = "Naam";
+                Listboxmaterialen.DataBind();
+            }
+        }
+
+        protected void Bevestig_Click(object sender, EventArgs e)
+        {
+            Response.Write(Session["Stap1"]);
+            Response.Write(Session["Stap2"]);
+            Response.Write(Session["Stap3"]);
+            Response.Write(Session["Stap4"]);
+            Session["Stap1"] = null;
+            Session["Stap2"] = null;
+            Session["Stap3"] = null;
+            Session["Stap4"] = null;
         }
     }
 }
